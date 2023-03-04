ce507ab7f3a22351bb92e8f6b8bb349875a38542
shaikhanas02 <122043549+shaikhanas02@users.noreply.github.com>
2023-03-04 16:37:14 +0530

    Update scriptanas.js

--- scriptanas.js
+++ scriptanas.js
@@ -41,8 +41,8 @@ const notes = JSON.parse(localStorage.getItem("notes") || "[]");
 let isUpdate = false, updateId;
 
 function deleteNote(noteId) {
-    let confirmDel = confirm("Are you sure you want to delete this note?");
-    if(!confirmDel) return;
+   // let confirmDel = confirm("Are you sure you want to delete this note?");
+    //if(!confirmDel) return;
     notes.splice(noteId, 1);
     localStorage.setItem("notes", JSON.stringify(notes));
     showNotes();
